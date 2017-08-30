5919e196a00d5c87dab3a2f1c4b3458b5457c75c
KaiserChan <chankaiserkk@gmail.com>
2017-08-30 17:52:30 -0400

    Updated routing-controller - Defined @header instance variable inside all instance methods in controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
+    @header = "Welcome to our page!"
   end
 
   def about
+    @header = "About us"
   end
 
   def contest
+    @header = "We have a contest!"
   end
 
 end
